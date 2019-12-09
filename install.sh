92dbb21d1731fbccce0b0b68e14025aaa695aae0
mit-d <mitt2309@vandals.uidaho.edu>
2019-12-09 19:09:51 +0000

    fixed stow script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "This script will set up the raspberry pi as SUNBEAM"
 
-echo "Installing binaries to /usr/local/bin" && stow -Rt /usr/local bin
+echo "Installing binaries to /usr/local/bin" && stow -Rt /usr/local/bin bin
 echo "Installing systemd services to /etc/systemd/system" && stow -Rt /etc/systemd/system system
 # ln -sT  monitor.py /usr/local/bin/monitor.py ||
 # ln -sT  ./etc/systemd/system/monitor_voltage.service /etc/systemd/system/monitor_voltage.service ||
