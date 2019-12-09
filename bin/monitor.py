f7bf1751c1f21066ff939103b88c5faae081fc2f
mit-d <mitt2309@vandals.uidaho.edu>
2019-12-09 19:04:25 +0000

    Initial Commit

---
+++ bin/monitor.py
@@ -0,0 +1,47 @@
+import time
+import busio
+import digitalio
+import board
+import adafruit_mcp3xxx.mcp3008 as MCP
+from adafruit_mcp3xxx.analog_in import AnalogIn
+
+# create the bus
+spi = busio.SPI(clock = board.SCK, MISO = board.MISO, MOSI = board.MOSI);
+
+# create the chip select
+cs = digitalio.DigitalInOut(board.D5)
+
+# create MCP object
+mcp = MCP.MCP3008(spi, cs)
+
+# create an analog input channel
+chan = [AnalogIn(mcp, MCP.P0), 
+        AnalogIn(mcp, MCP.P1), 
+        AnalogIn(mcp, MCP.P2), 
+        AnalogIn(mcp, MCP.P3), 
+        AnalogIn(mcp, MCP.P4), 
+        AnalogIn(mcp, MCP.P5), 
+        AnalogIn(mcp, MCP.P6), 
+        AnalogIn(mcp, MCP.P7)]
+
+
+
+f = open("/tmp/MCP3008_v0", "w+")
+
+while 1:
+    f.write(str(chan[0].voltage) + ' Vdc,\t')
+    f.write(str(chan[1].voltage) + ' Vdc,\t')
+    f.write(str(chan[2].voltage) + ' Vdc,\t')
+    f.write(str(chan[3].voltage) + ' Vdc,\t')
+    f.write(str(chan[4].voltage) + ' Vdc,\t')
+    f.write(str(chan[5].voltage) + ' Vdc,\t')
+    f.write(str(chan[6].voltage) + ' Vdc,\t')
+    f.write(str(chan[7].voltage) + ' Vdc\n')
+    time.sleep(1)
+#    print(str(chan.voltage) + ' Vdc', end='\r')
+#    i = chan.value
+#    while(i > 0):
+#        print('#', end='')
+#        i -= 111;
+#    print('')
+#    time.sleep(.001)
---
+++ server/server.php
@@ -0,0 +1,22 @@
+//
+// Will create a UDP server on port 10000
+//
+
+<?php
+
+$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+socket_bind($sock, '0.0.0.0', 10000);
+
+// $voltages = fopen("/tmp/MCP3008_v0", "r") or die("Unable to open /tmp/MCP3008_v0");
+$file = file("/tmp/MCP3008_v0");
+
+for(;;){
+	socket_recvfrom($sock, $message, 1024, 0, $ip, $port);
+	// $reply = str_rot13($message);
+	// $reply = $fread($voltages);
+	for($i = max(0,count($file-6)); $i < count($file); $i++) {
+		$reply = $file[$i] . \n;
+		socket_sendto($sock, $reply, strlen($reply), 0, $ip, $port);
+	}
+}
+fclose($voltages);
