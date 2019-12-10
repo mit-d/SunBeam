6b4a97f1b8efd28f6c7ea68b4572d1e6a0bdaa97
Derek Mitten <mitd@oOOoOo.local>
2019-12-10 12:25:39 -0800

    Changed structure to be more linux-like, updated install script to reflect that

--- bin/server.php
+++ bin/server.php
@@ -1,10 +1,9 @@
-//
-// Will create a UDP server on port 10000
-//
+/* Will create a UDP server on port 10000 */
 
 <?php
-
 $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+
+/* UDP port 10000 */
 socket_bind($sock, '0.0.0.0', 10000);
 
 // $voltages = fopen("/tmp/MCP3008_v0", "r") or die("Unable to open /tmp/MCP3008_v0");
@@ -12,11 +11,11 @@ $file = file("/tmp/MCP3008_v0");
 
 for(;;){
 	socket_recvfrom($sock, $message, 1024, 0, $ip, $port);
-	// $reply = str_rot13($message);
 	// $reply = $fread($voltages);
 	for($i = max(0,count($file-6)); $i < count($file); $i++) {
 		$reply = $file[$i] . \n;
 		socket_sendto($sock, $reply, strlen($reply), 0, $ip, $port);
 	}
 }
+/* never reached */
 fclose($voltages);
--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 echo "This script will set up the raspberry pi as SUNBEAM"
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 
 echo "Installing binaries to /usr/local/bin" && stow -Rt /usr/local/bin bin
+echo "Installing files to /usr/local/share" && stow -Rt /usr/local/share share
 echo "Installing systemd services to /etc/systemd/system" && stow -Rt /etc/systemd/system system
-# ln -sT  monitor.py /usr/local/bin/monitor.py ||
-# ln -sT  ./etc/systemd/system/monitor_voltage.service /etc/systemd/system/monitor_voltage.service ||
-# ln -sT  ./etc/systemd/system/sunbeam_php_server.service /etc/systemd/system/sunbeam_php_server.service
 
